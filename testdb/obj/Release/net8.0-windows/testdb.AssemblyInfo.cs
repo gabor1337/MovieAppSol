2fe66ba2e7557531d8e0cfc65ab3176a99aa580b
unknown <18molnargabor18@gmail.com>
2025-04-18 11:44:19 +0200

    MovieDB

--- testdb/obj/Release/net8.0-windows/testdb.AssemblyInfo.cs
+++ testdb/obj/Release/net8.0-windows/testdb.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("testdb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d29e0327f623dae95aa70bba514de4b20cd1fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("testdb")]
